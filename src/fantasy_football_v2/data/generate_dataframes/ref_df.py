153a457f9057aadf9ea89809d49034ffec1a38e9
violetdiamonds <violetdiamonds@protonmail.com>
2025-09-26 13:10:26 -0400

    changing default years to if statement

--- src/fantasy_football_v2/data/generate_dataframes/ref_df.py
+++ src/fantasy_football_v2/data/generate_dataframes/ref_df.py
@@ -6,8 +6,13 @@ import nfl_data_py as nfl
 
 def generate_ref_df(
     # default to past 5 years including current year
-    years: List[int] = [datetime.today().year - i for i in range(5)]
+    years: List[int] = None
 ):
+    # Default years to the past 5 years, including current year
+    if years is None:
+        current_year = datetime.today().year
+        years = [current_year - i for i in range(5)]
+
     # Get data from nfl_data_py
     df = nfl.import_officials(
         years=years,
