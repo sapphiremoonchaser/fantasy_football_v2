deb51b56cbaa7bc82c90dd5091318bc5fdd58d1a
violetdiamonds <violetdiamonds@protonmail.com>
2025-09-26 13:08:15 -0400

    return df to inspect

--- src/fantasy_football_v2/data/generate_dataframes/ref_df.py
+++ src/fantasy_football_v2/data/generate_dataframes/ref_df.py
@@ -1,3 +1,16 @@
 """Generate ref dataframe
 """
+from typing import List
+from datetime import datetime
 import nfl_data_py as nfl
+
+def generate_ref_df(
+    # default to past 5 years including current year
+    years: List[int] = [datetime.today().year - i for i in range(5)]
+):
+    # Get data from nfl_data_py
+    df = nfl.import_officials(
+        years=years,
+    )
+
+    return df
