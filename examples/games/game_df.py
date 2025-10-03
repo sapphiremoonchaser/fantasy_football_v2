9af5a2cdf9b668ceb77506ed37d8d64e15de34b6
violetdiamonds <violetdiamonds@protonmail.com>
2025-10-03 17:02:31 -0400

    referee analysis

--- examples/games/game_df.py
+++ examples/games/game_df.py
@@ -2,7 +2,7 @@
 """
 from src.fantasy_football_v2.data.generate_dataframes.generate_game_df import generate_game_df
 
-# Dataframe of current refs
+# Dataframe of games
 game_df = generate_game_df(
     years=[2021, 2022, 2023, 2024, 2025]
 ).reindex()
