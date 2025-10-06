f4a67e573e6b4228d056f413b64af29d4ad99561
violetdiamonds <violetdiamonds@protonmail.com>
2025-10-06 15:02:41 -0400

    generating next-gen-stats passing df

---
+++ src/fantasy_football_v2/data/generate_dataframes/generate_passer_df.py
@@ -0,0 +1,55 @@
+"""Generate passing data dataframe
+"""
+from typing import List
+from datetime import datetime
+import nfl_data_py as nfl
+
+def generate_pass_pbp_df(
+    # default to current year
+    years: List[int]=None
+):
+    # Default column list
+    columns = [
+        'season',
+        'week',
+        'player_gsis_id',
+        'player_display_name',
+        'player_position', # Filter to 'QB'
+        'team_abbr',
+        'avg_time_to_throw',
+        'aggressiveness',
+        'max_completed_air_distance',
+        'attempts',
+        'pass_yards',
+        'pass_touchdowns',
+        'interceptions',
+        'passer_rating',
+        'completions',
+        'completion_percentage',
+        'avg_air_distance',
+        'max_air_distance'
+    ]
+
+    # Default years to current year
+    if years is None:
+        current_month = datetime.today().month
+        if current_month in range(3,14):
+            years = datetime.today().year
+        else:
+            years = datetime.today().year - 1
+
+    # Get data frame from nfl_data_py
+    df = nfl.import_ngs_data(
+        stat_type='passing',
+        years=years,
+    )
+
+    # Filter to regular season games
+    df = df[
+        df['season_type'] == 'REG'
+    ]
+
+    # Drop un-needed columns
+    df = df[columns]
+
+    return df
