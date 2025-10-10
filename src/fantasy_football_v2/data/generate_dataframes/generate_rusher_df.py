effdfad02399e06288c8bc4270b6e065d926540e
violetdiamonds <violetdiamonds@protonmail.com>
2025-10-10 10:41:18 -0400

    generate rusher df

--- src/fantasy_football_v2/data/generate_dataframes/generate_rusher_df.py
+++ src/fantasy_football_v2/data/generate_dataframes/generate_rusher_df.py
@@ -4,6 +4,8 @@ from typing import List
 from datetime import datetime
 import nfl_data_py as nfl
 
+from src.fantasy_football_v2.utilities.time_functions import get_current_season
+
 def generate_rusher_df(
     # default to current year
     years: List[int] = None
@@ -11,7 +13,34 @@ def generate_rusher_df(
     # Default column list
     columns = [
         'season',
-        'week'
+        'week',
+        'player_gsis_id',
+        'player_display_name',
+        'player_position',
+        'team_abbr',
+        'efficiency',
+        'rush_attempts',
+        'rush_yards',
+        'avg_rush_yards',
+        'rush_touchdowns'
     ]
 
     # Default years to current year
+    if years is None:
+        years = get_current_season()
+
+    # Get data from nfl_data_py
+    df = nfl.import_ngs_data(
+        stat_type='rushing',
+        years=years,
+    )
+
+    # Filter to regular season games
+    df = df[
+        df['season_type'] == 'REG'
+    ]
+
+    # Drop un-needed columns
+    df = df[columns]
+
+    return df
